2c4d19bdfc9b0f966e3369baf7d863e46a8706e5
HenryArin <128102839+HenryArin@users.noreply.github.com>
2026-03-03 23:15:03 -0800

    chore: minor style updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import "./styles/theme.css";
+import "./styles/typography.css";
 import "./styles/base.css";
 
 createRoot(document.getElementById('root')!).render(
